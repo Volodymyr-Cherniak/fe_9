8c5f06079c1cf06659728df6293d8480fa3317e4
volodymyr <you@example.com>
2021-01-16 17:49:05 +0200

    upd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Display from './Display';
 
 function App() {
-  return (
-    <h2>Hello</h2>
-  );
+    const state = [
+        {id: 1, name: 'Alice', lastName: 'Murrey', age: 22, sity: 'LA', stats: {active: true}},
+        {id: 1, name: 'Bob', lastName: 'Murrey', age: 25, sity: 'SA', stats: {active: true}},
+        {id: 1, name: 'Rayan', lastName: 'Smith', age: 30, sity: 'Lviv', stats: {active: true}},
+    ];
+
+    const date = new Date().getFullYear();
+    const people = 'New students';
+
+
+    return (
+        <div>
+            <h2>{people + ' in ' + date} </h2>
+            <p>{state.map(el => <h4>{`${el.name} ${el.lastName}, has ${el.age}, live in ${el.sity}`}</h4>)}</p>
+
+            <div>hello world</div>
+            <Display/>
+            <ul>
+                <li>first</li>
+                <li>second</li>
+                <li>third</li>
+                <li>fourth</li>
+                <li>
+                <video src="https://www.youtube.com/watch?v=E7_U8eJdVY4">video</video>
+                </li>
+
+            </ul>
+        </div>
+    )
 }
 
 export default App;
